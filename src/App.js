024c3c12170acbc58b74ef71cefccfb24d479950
Jeet Veer <jeetveer@gmail.com>
2024-11-20 15:38:48 +0530

    ha

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
           <Route path='/register' element={<Register/>}/>
           <Route path='/edit:id' element={<Edit/>} />
           <Route path='/userprofile/:id' element={<Profile/>}/>
-          {/* <Route path='/userprofile/:id' element={<Profile/>}/> */}
         </Routes>
       </>
     );
